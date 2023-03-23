69d059f2242f40fe5bafa5b3b17b83357afcdd2d
Khaled Saleh <khaled@connou.app>
2023-03-23 14:20:00 +0100

    writing the pageview for the onboarding

--- lib/modules/on_boarding/on_boarding_screen.dart
+++ lib/modules/on_boarding/on_boarding_screen.dart
@@ -8,8 +8,8 @@ class OnBoardingScreen extends StatelessWidget {
 
   List<BoardingModel> boarding = [
     BoardingModel('Title 1', 'Body 1', 'assets/images/shopping-bag.png'),
-    BoardingModel('Title 2', 'Body 2', 'assets/images/shopping-bag.png'),
-    BoardingModel('Title 3', 'Body 3', 'assets/images/shopping-bag.png'),
+    BoardingModel('Title 2', 'Body 2', 'assets/images/basket.png'),
+    BoardingModel('Title 3', 'Body 3', 'assets/images/groceries.png'),
   ];
 
   @override
@@ -17,8 +17,65 @@ class OnBoardingScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        children: [],
+        children: [
+          Expanded(
+            child: PageView.builder(
+              itemCount: boarding.length,
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: buildBoardingItem(boarding[index]),
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Row(
+              children: [
+                Text("Test"),
+                Spacer(),
+                FloatingActionButton(
+                  child: Icon(Icons.arrow_forward_ios_rounded),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget buildBoardingItem(BoardingModel model) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Image.asset(model.image),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Text(
+          model.title,
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          model.body,
+          style: TextStyle(
+            fontSize: 21,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 }
