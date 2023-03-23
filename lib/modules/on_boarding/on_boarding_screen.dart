0ec72bb447ea21457427304683dd9f259c19377e
Khaled Saleh <khaled@connou.app>
2023-03-23 14:52:42 +0100

    adding floating action buttons, textbutton in the appbar, smoothpage indicator, and configuring the page view

--- lib/modules/on_boarding/on_boarding_screen.dart
+++ lib/modules/on_boarding/on_boarding_screen.dart
@@ -2,6 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:shop_app/models/boarding_mode.dart';
+import 'package:shop_app/modules/login/login_screen.dart';
+import 'package:shop_app/shared/components/constants.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   OnBoardingScreen({Key key}) : super(key: key);
@@ -11,17 +14,47 @@ class OnBoardingScreen extends StatelessWidget {
     BoardingModel('Title 2', 'Body 2', 'assets/images/basket.png'),
     BoardingModel('Title 3', 'Body 3', 'assets/images/groceries.png'),
   ];
-
+  var boardController = PageController();
+  bool isLastPage = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return LoginScreen();
+                  },
+                ),
+                (Route<dynamic> route) => false,
+              );
+            },
+            child: Text(
+              "SKIP",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+            ),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           Expanded(
             child: PageView.builder(
+              controller: boardController,
               itemCount: boarding.length,
-              physics: BouncingScrollPhysics(),
+              physics: new NeverScrollableScrollPhysics(),
+              onPageChanged: (value) {
+                print(value);
+                if (value == boarding.length - 1) {
+                  isLastPage = true;
+                } else {
+                  isLastPage = false;
+                }
+              },
               itemBuilder: (context, index) {
                 return Padding(
                   padding: const EdgeInsets.all(30.0),
@@ -30,18 +63,56 @@ class OnBoardingScreen extends StatelessWidget {
               },
             ),
           ),
-          SizedBox(
-            height: 30,
-          ),
           Padding(
             padding: const EdgeInsets.all(30.0),
             child: Row(
               children: [
-                Text("Test"),
+                SmoothPageIndicator(
+                  controller: boardController,
+                  count: boarding.length,
+                  effect: ExpandingDotsEffect(
+                    dotColor: Colors.grey,
+                    dotHeight: 15,
+                    dotWidth: 15,
+                    activeDotColor: defaultColor,
+                    expansionFactor: 4,
+                  ),
+                ),
                 Spacer(),
                 FloatingActionButton(
+                  heroTag: "previous",
+                  child: Icon(Icons.arrow_back_ios_rounded),
+                  onPressed: () {
+                    boardController.previousPage(
+                      duration: Duration(milliseconds: 1500),
+                      curve: Curves.fastLinearToSlowEaseIn,
+                    );
+                  },
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                FloatingActionButton(
+                  heroTag: "next",
                   child: Icon(Icons.arrow_forward_ios_rounded),
-                  onPressed: () {},
+                  onPressed: () {
+                    if (isLastPage) {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return LoginScreen();
+                          },
+                        ),
+                        (Route<dynamic> route) => false,
+                      );
+                    } else {
+                      boardController.nextPage(
+                        duration: Duration(milliseconds: 1000),
+                        curve: Curves.fastLinearToSlowEaseIn,
+                      );
+                    }
+                  },
                 ),
               ],
             ),
