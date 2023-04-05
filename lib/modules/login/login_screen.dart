3113224e43963960f99c876c757ad4e2fbab3ab0
Khaled Saleh <khaled@connou.app>
2023-04-05 18:54:56 +0200

    editing the validators and style the login title

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -17,60 +17,74 @@ class LoginScreen extends StatelessWidget {
               builder: (context, state) {
                 LoginHandler loginHandler =
                     BlocProvider.of<LoginHandler>(context);
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text("LOGIN"),
-                    Text(
-                      "Jump to our exclusive offers!!",
-                      style: const TextStyle(
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    defaultFormField(
-                      context: context,
-                      controller: loginHandler.emailController,
-                      type: TextInputType.emailAddress,
-                      validate: () {},
-                      label: 'Email',
-                      prefix: Icons.email_outlined,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    defaultFormField(
-                      context: context,
-                      controller: loginHandler.passwordController,
-                      isPassword: true,
-                      type: TextInputType.emailAddress,
-                      validate: () {},
-                      label: 'Password',
-                      prefix: Icons.email_outlined,
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "LOGIN",
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Text("Don't have an account?"),
-                        TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            "REGISTER",
-                          ),
+                return Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("LOGIN",
+                          style: const TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      Text(
+                        "Jump to our exclusive offers!!",
+                        style: const TextStyle(
+                          fontStyle: FontStyle.italic,
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      defaultFormField(
+                        context: context,
+                        controller: loginHandler.emailController,
+                        type: TextInputType.emailAddress,
+                        validate: (value) {
+                          if (value.isEmpty) {
+                            return 'Please provide your email';
+                          }
+                        },
+                        label: 'Email',
+                        prefix: Icons.email_outlined,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      defaultFormField(
+                        context: context,
+                        controller: loginHandler.passwordController,
+                        isPassword: true,
+                        type: TextInputType.emailAddress,
+                        validate: (value) {
+                          if (value.isEmpty) {
+                            return 'Password cannot be empty';
+                          }
+                        },
+                        label: 'Password',
+                        prefix: Icons.email_outlined,
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      defaultButton(
+                        function: () {},
+                        text: "LOGIN",
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Don't have an account?"),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "REGISTER",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 );
               },
             ),
