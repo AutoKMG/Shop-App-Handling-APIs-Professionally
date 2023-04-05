d05ba628b18dd730d18eff9b4928e2d55a2b9aff
Khaled Saleh <khaled@connou.app>
2023-04-05 21:04:07 +0200

    adding changing the language of the response in postData method

--- lib/shared/network/remote/dio_helper.dart
+++ lib/shared/network/remote/dio_helper.dart
@@ -21,11 +21,16 @@ class DioHelper {
     return await dio.get(url, queryParameters: query);
   }
 
-  static Future<Response> postData({
-    @required String url,
-    Map<String, dynamic> query,
-    @required Map<String, dynamic> data,
-  }) async {
+  static Future<Response> postData(
+      {@required String url,
+      Map<String, dynamic> query,
+      @required Map<String, dynamic> data,
+      String lang = 'ar',
+      String token}) async {
+    dio.options.headers = {
+      'lang': lang,
+      'Authorization': token,
+    };
     return await dio.post(
       url,
       queryParameters: query,
