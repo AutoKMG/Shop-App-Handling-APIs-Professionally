b5cda2b52c113057de0efe7792a5849cdc5fcae7
Khaled Saleh <khaled@connou.app>
2023-04-05 19:01:02 +0200

    writing the method of signing in

--- lib/shared/logic/login/cubit/handler.dart
+++ lib/shared/logic/login/cubit/handler.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+import 'package:shop_app/shared/network/end_points.dart';
+import 'package:shop_app/shared/network/remote/dio_helper.dart';
 
 part 'state.dart';
 
@@ -9,6 +11,22 @@ class LoginHandler extends Cubit<LoginState> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   bool isPasswordVisible = false;
+
+  void userLogin({
+    @required String email,
+    @required String password,
+  }) {
+    emit(LoginStateLoading());
+    DioHelper.postData(url: LOGIN, data: {
+      'email': email,
+      'password': password,
+    }).then((value) {
+      print(value);
+      emit(LoginStateSuccessful());
+    }).catchError((error) {
+      emit(LoginStateError(error.toString()));
+    });
+  }
 
   void changePasswordVisibility() {
     isPasswordVisible = !isPasswordVisible;
