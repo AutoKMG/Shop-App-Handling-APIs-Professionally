54a021ea8d3d62575a8086a51f30e81ce754b6e8
Khaled Saleh <khaled@connou.app>
2023-04-05 20:55:03 +0200

    using conditional builder around login button and wrapping the widgets with Form

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_app/shared/components/components.dart';
 import 'package:shop_app/shared/logic/login/cubit/handler.dart';
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key key}) : super(key: key);
@@ -19,71 +20,84 @@ class LoginScreen extends StatelessWidget {
                     BlocProvider.of<LoginHandler>(context);
                 return Padding(
                   padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("LOGIN",
+                  child: Form(
+                    key: loginHandler.formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("LOGIN",
+                            style: const TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        Text(
+                          "Jump to our exclusive offers!!",
                           style: const TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                          )),
-                      Text(
-                        "Jump to our exclusive offers!!",
-                        style: const TextStyle(
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      defaultFormField(
-                        context: context,
-                        controller: loginHandler.emailController,
-                        type: TextInputType.emailAddress,
-                        validate: (value) {
-                          if (value.isEmpty) {
-                            return 'Please provide your email';
-                          }
-                        },
-                        label: 'Email',
-                        prefix: Icons.email_outlined,
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      defaultFormField(
-                        context: context,
-                        controller: loginHandler.passwordController,
-                        isPassword: true,
-                        type: TextInputType.emailAddress,
-                        validate: (value) {
-                          if (value.isEmpty) {
-                            return 'Password cannot be empty';
-                          }
-                        },
-                        label: 'Password',
-                        prefix: Icons.email_outlined,
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      defaultButton(
-                        function: () {},
-                        text: "LOGIN",
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Don't have an account?"),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              "REGISTER",
-                            ),
+                            fontStyle: FontStyle.italic,
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        defaultFormField(
+                          context: context,
+                          controller: loginHandler.emailController,
+                          type: TextInputType.emailAddress,
+                          validate: (value) {
+                            if (value.isEmpty) {
+                              return 'Please provide your email';
+                            }
+                          },
+                          label: 'Email',
+                          prefix: Icons.email_outlined,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        defaultFormField(
+                          context: context,
+                          controller: loginHandler.passwordController,
+                          isPassword: true,
+                          type: TextInputType.emailAddress,
+                          validate: (value) {
+                            if (value.isEmpty) {
+                              return 'Password cannot be empty';
+                            }
+                          },
+                          label: 'Password',
+                          prefix: Icons.email_outlined,
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        ConditionalBuilder(
+                          condition: state is! LoginStateLoading,
+                          builder: (context) {
+                            return defaultButton(
+                              function: () {},
+                              text: "LOGIN",
+                            );
+                          },
+                          fallback: (context) {
+                            return Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          },
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Don't have an account?"),
+                            TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "REGISTER",
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
