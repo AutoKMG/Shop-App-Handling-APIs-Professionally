ae6adf597c793f8003c59751911c4f1f85d08801
Khaled Saleh <khaled@connou.app>
2023-04-08 16:11:22 +0200

    making the query parameter unrequired

--- lib/shared/network/remote/dio_helper.dart
+++ lib/shared/network/remote/dio_helper.dart
@@ -16,7 +16,7 @@ class DioHelper {
 
   static Future<Response> getData({
     @required String url,
-    @required Map<String, dynamic> query,
+    Map<String, dynamic> query,
   }) async {
     return await dio.get(url, queryParameters: query);
   }
