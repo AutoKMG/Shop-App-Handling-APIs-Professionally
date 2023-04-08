ebed5792fbed46948d253619cd255f59c1a35d0c
Khaled Saleh <khaled@connou.app>
2023-04-08 15:40:07 +0200

    changing shopstate to mainshopstate

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => MainShopHandler(),
-      child: BlocConsumer<MainShopHandler, ShopState>(
+      child: BlocConsumer<MainShopHandler, MainShopState>(
         listener: (context, state) {
           // TODO: implement listener
         },
