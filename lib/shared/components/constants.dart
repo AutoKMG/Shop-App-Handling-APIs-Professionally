3ce18100b9f5c5c39c33e41180ec73a770110166
Khaled Saleh <khaled@connou.app>
2023-04-08 15:39:27 +0200

    making the signout function in a global file

--- lib/shared/components/constants.dart
+++ lib/shared/components/constants.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/modules/login/login_screen.dart';
+import 'package:shop_app/shared/network/local/cache_helper.dart';
 
 Color defaultColor = Colors.blue;
+
+void signOut(context) {
+  CacheHelper.removeData(key: 'token').then((value) {
+    if (value) {
+      Navigator.pushAndRemoveUntil(context,
+          MaterialPageRoute(builder: (context) {
+        return LoginScreen();
+      }), (route) => false);
+    }
+  });
+}
