7d6ccdc83e10c5ef210631203dc56d43b8571287
Khaled Saleh <khaled@connou.app>
2023-04-08 15:47:44 +0200

    adding bottomnavbar components as screens, current index, and toggling method

--- lib/layout/shop_layout.dart
+++ lib/layout/shop_layout.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_app/modules/login/login_screen.dart';
+import 'package:shop_app/modules/search/search_screen.dart';
+import 'package:shop_app/shared/components/constants.dart';
+import 'package:shop_app/shared/logic/shop/handler.dart';
 import 'package:shop_app/shared/network/local/cache_helper.dart';
 
 class ShopLayout extends StatelessWidget {
@@ -7,23 +11,63 @@ class ShopLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Shoppy")),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            CacheHelper.removeData(key: 'token').then((value) {
-              if (value) {
-                Navigator.pushAndRemoveUntil(context,
-                    MaterialPageRoute(builder: (context) {
-                  return LoginScreen();
-                }), (route) => false);
-              }
-            });
-          },
-          child: Text("Sign Out"),
+    return BlocBuilder<MainShopHandler, MainShopState>(
+        builder: (context, state) {
+      MainShopHandler mainShopHandler =
+          BlocProvider.of<MainShopHandler>(context);
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Shoppy"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SearchScreen();
+                    },
+                  ),
+                );
+              },
+              icon: Icon(
+                Icons.search,
+              ),
+            ),
+          ],
         ),
-      ),
-    );
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: mainShopHandler.currentIndex,
+          onTap: mainShopHandler.changeCurrentBottomNavIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.apps,
+              ),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+              ),
+              label: 'Favorties',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings,
+              ),
+              label: 'Settings',
+            ),
+          ],
+        ),
+        body: mainShopHandler.screens[mainShopHandler.currentIndex],
+      );
+    });
   }
 }
--- lib/shared/logic/shop/handler.dart
+++ lib/shared/logic/shop/handler.dart
@@ -1,14 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_app/layout/shop_layout.dart';
+import 'package:shop_app/modules/categories/categories_screen.dart';
+import 'package:shop_app/modules/favorites/favorites_screen.dart';
 import 'package:shop_app/modules/login/login_screen.dart';
 import 'package:shop_app/modules/on_boarding/on_boarding_screen.dart';
+import 'package:shop_app/modules/products/products_screen.dart';
+import 'package:shop_app/modules/settings/settings_screen.dart';
 import 'package:shop_app/shared/network/local/cache_helper.dart';
 
 part 'state.dart';
 
-class MainShopHandler extends Cubit<ShopState> {
-  MainShopHandler() : super(ShopStateInitial()) {
+class MainShopHandler extends Cubit<MainShopState> {
+  MainShopHandler() : super(MainShopStateInitial()) {
     isDark = CacheHelper.getData(key: 'isDark') ?? false;
     isRTL = CacheHelper.getData(key: 'isRTL') ?? false;
     isOnBoardingDone = CacheHelper.getData(key: 'isOnBoardingDone') ?? false;
@@ -27,6 +31,13 @@ class MainShopHandler extends Cubit<ShopState> {
   bool isOnBoardingDone;
   String token;
   Widget displayedWidget;
+  int currentIndex = 0;
+  List<Widget> screens = [
+    ProductsScreen(),
+    CategoriesScreen(),
+    FavrotiesScreen(),
+    SettingsScreen(),
+  ];
 
   List<BottomNavigationBarItem> bottomNavItems = [
     BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Business'),
@@ -38,14 +49,19 @@ class MainShopHandler extends Cubit<ShopState> {
   void changeDirection() {
     isRTL = !isRTL;
     CacheHelper.putData(key: 'isRTL', value: isRTL).then((_) {
-      emit(ShopStateToggleDirectionality());
+      emit(MainShopStateToggleDirectionality());
     });
   }
 
   void toggleDarkTheme() {
     isDark = !isDark;
     CacheHelper.putData(key: 'isDark', value: isDark).then((_) {
-      emit(ShopStateToggleDarkTheme());
+      emit(MainShopStateToggleDarkTheme());
     });
+  }
+
+  void changeCurrentBottomNavIndex(int index) {
+    currentIndex = index;
+    emit(MainShopStateBottomNavBarIndexChanged());
   }
 }
