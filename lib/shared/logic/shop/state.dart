9edf764f3ffbeec85384ead3225ee207d951e292
Khaled Saleh <khaled@connou.app>
2023-04-08 16:12:11 +0200

    adding three states for retrieving products: success, loading, and error

--- lib/shared/logic/shop/state.dart
+++ lib/shared/logic/shop/state.dart
@@ -10,3 +10,12 @@ class MainShopStateToggleDirectionality extends MainShopState {}
 class MainShopStateToggleDarkTheme extends MainShopState {}
 
 class MainShopStateBottomNavBarIndexChanged extends MainShopState {}
+
+class MainShopStateProductsRetrieveDone extends MainShopState {}
+
+class MainShopStateProductsRetrieveLoading extends MainShopState {}
+
+class MainShopStateProductsRetrieveError extends MainShopState {
+  final String error;
+  MainShopStateProductsRetrieveError(this.error);
+}
