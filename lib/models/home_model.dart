e71aae60d81c67a075bb66fe1907660c7a817bc0
Khaled Saleh <khaled@connou.app>
2023-04-08 16:08:16 +0200

    the class model to use for the products

---
+++ lib/models/home_model.dart
@@ -0,0 +1,57 @@
+class HomeModel {
+  String status;
+  HomeDataModel data;
+
+  HomeModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    data = HomeDataModel.fromJson(json['data']);
+  }
+}
+
+class HomeDataModel {
+  List<BannerModel> banners = [];
+  List<ProductModel> products = [];
+
+  HomeDataModel.fromJson(Map<String, dynamic> json) {
+    json['banners'].forEach((element) {
+      banners.add(element);
+    });
+    json['products'].forEach((element) {
+      products.add(element);
+    });
+  }
+}
+
+class BannerModel {
+  int id;
+  String image;
+
+  BannerModel.fromJson(Map<String, dynamic> json) {
+    this.id = json['id'];
+    this.image = json['image'];
+  }
+}
+
+class ProductModel {
+  int id;
+  dynamic price;
+  dynamic oldPrice;
+  dynamic discount;
+  String image;
+  String name;
+  List<String> images;
+  bool inFavorties;
+  bool inCart;
+
+  ProductModel.fromJson(Map<String, dynamic> json) {
+    this.id = json['id'];
+    this.price = json['price'];
+    this.oldPrice = json['old_price'];
+    this.discount = json['discount'];
+    this.image = json['image'];
+    this.name = json['name'];
+    this.images = json['images'];
+    this.inFavorties = json['in_favorties'];
+    this.inCart = json['in_cart'];
+  }
+}
