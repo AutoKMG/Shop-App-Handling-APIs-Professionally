23369dca8836b05b4d674e3ec03a49770c5d1196
Khaled Saleh <khaled@connou.app>
2023-04-09 21:00:39 +0200

    adding language change option

--- lib/modules/settings/settings_screen.dart
+++ lib/modules/settings/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/shared/components/constants.dart';
+import 'package:shop_app/shared/network/local/cache_helper.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key key}) : super(key: key);
@@ -7,11 +8,41 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          signOut(context);
-        },
-        child: Text("Sign Out"),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Text(
+                  'Language',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                Spacer(),
+                DropdownMenu(
+                  enableSearch: false,
+                  initialSelection: CacheHelper.getData(key: 'lang'),
+                  dropdownMenuEntries: [
+                    DropdownMenuEntry(value: 'ar', label: 'Arabic'),
+                    DropdownMenuEntry(value: 'en', label: 'English')
+                  ],
+                  onSelected: (value) {
+                    CacheHelper.putData(key: 'lang', value: value);
+                  },
+                  leadingIcon: Icon(Icons.language),
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              signOut(context);
+            },
+            child: Text("Sign Out"),
+          ),
+        ],
       ),
     );
   }
