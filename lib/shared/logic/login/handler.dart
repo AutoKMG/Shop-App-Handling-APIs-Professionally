afa26a7484cac999bb6ca944d6df5888d636cccd
Khaled Saleh <khaled@connou.app>
2023-04-19 18:26:52 +0200

    passing the lang parameter from the cachehelper

--- lib/shared/logic/login/handler.dart
+++ lib/shared/logic/login/handler.dart
@@ -2,30 +2,38 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_app/models/shop_login_model.dart';
 import 'package:shop_app/shared/network/end_points.dart';
+import 'package:shop_app/shared/network/local/cache_helper.dart';
 import 'package:shop_app/shared/network/remote/dio_helper.dart';
 
 part 'state.dart';
 
 class LoginHandler extends Cubit<LoginState> {
-  LoginHandler() : super(LoginStateInitial());
+  LoginHandler() : super(LoginStateInitial()) {
+    lang = CacheHelper.getData(key: 'lang') ?? 'en';
+  }
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   ShopLoginModel shopLoginModel;
+  String lang;
   var formKey = GlobalKey<FormState>();
   bool isPasswordHidden = true;
   IconData suffixIcon = Icons.visibility;
   void userLogin() {
     emit(LoginStateLoading());
-    DioHelper.postData(url: LOGIN, data: {
-      'email': emailController.text,
-      'password': passwordController.text,
-    }).then((value) {
+    DioHelper.postData(
+      url: LOGIN,
+      data: {
+        'email': emailController.text,
+        'password': passwordController.text,
+      },
+      lang: lang,
+    ).then((value) {
       emailController.clear();
       passwordController.clear();
       shopLoginModel = ShopLoginModel.fromJson(value.data);
       emit(LoginStateSuccessful(shopLoginModel));
     }).catchError((_) {
-      emit(LoginStateError(shopLoginModel.messgae));
+      emit(LoginStateError(shopLoginModel.message));
     });
   }
 
