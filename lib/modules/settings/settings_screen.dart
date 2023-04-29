e007c26904d2a93aebaba10b15ce1c041661bc6d
Khaled Saleh <khaled@connou.app>
2023-04-29 15:34:12 +0200

    ignoring must_be_immutable

--- lib/modules/settings/settings_screen.dart
+++ lib/modules/settings/settings_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
