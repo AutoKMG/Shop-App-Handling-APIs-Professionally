91a946cf8009d3ab1ae3f457f94dbd9d5983cddb
Khaled Saleh <khaled@connou.app>
2023-04-29 19:10:21 +0200

    writing the search bar and displaying products

--- lib/modules/search/search_screen.dart
+++ lib/modules/search/search_screen.dart
@@ -1,7 +1,15 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/shared/components/components.dart';
+import 'package:shop_app/shared/logic/search/handler.dart';
+import 'package:shop_app/shared/logic/search/state.dart';
+import 'package:shop_app/shared/logic/shop/handler.dart';
 
 class SearchScreen extends StatelessWidget {
-  const SearchScreen({Key key}) : super(key: key);
+  SearchScreen({Key key}) : super(key: key);
+  var formKey = GlobalKey<FormState>();
+  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +17,50 @@ class SearchScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Search"),
       ),
-      body: Center(
-        child: Text(
-          "Search Screen",
+      body: BlocProvider(
+        create: (context) => SearchHandler(),
+        child: BlocBuilder<SearchHandler, SearchStates>(
+          builder: (context, state) {
+            SearchHandler searchHandler = context.read();
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  defaultFormField(
+                    context: context,
+                    controller: searchController,
+                    type: TextInputType.name,
+                    validate: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please enter something to search about';
+                      }
+                      return null;
+                    },
+                    label: 'Search',
+                    prefix: Icons.search,
+                    onSubmit: searchHandler.searchProducts,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  if (state is SearchStatesLoading) LinearProgressIndicator(),
+                  if (searchHandler.searchModel != null)
+                    Expanded(
+                      child: ListView.builder(
+                        itemBuilder: (context, index) {
+                          return gridProductBuilder(
+                              searchHandler.searchModel.data.productData[index],
+                              BlocProvider.of<MainShopHandler>(context),
+                              isOldPrice: false);
+                        },
+                        itemCount:
+                            searchHandler.searchModel.data.productData.length,
+                      ),
+                    ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
